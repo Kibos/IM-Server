b07f933214b406360e8ce53b7d82eff04ff438e5
rick <hitxyw@126.com>
2014-03-14 17:31:44 +0800

    为 group_subscriber.js 增加流程说明。

--- api/group_subscriber.js
+++ api/group_subscriber.js
@@ -1,6 +1,10 @@
-/*
+/**
  * 工作流程 --
- *    
+ * 	启动之后，获取所有的群组信息。（稍后可修改为各group subscriber manager服务器 
+ * 	sub到一个自己的channel，有一个group center，获取所有的群id，然后分发给不同的manager，这样对manager就变成透明。）
+ * 	根据一致性哈希来判断该群消息是否需要该manager来处理：
+ * 	如果需要的话，那么就new一个GroupSubscriber，来转发该群的所有消息。
+ * 	如果不需要的话，直接跳过，会有其他服务器来处理该群的消息。
  */
 
 var redis = require('redis');
@@ -21,7 +25,11 @@ function GroupSubscriber (gid) {
 		var gid = channel.replace("g::", "");
 		var groupUsers = [];
 		for (var i = 0; i < groupUsers.length; i++) {
+			/**
+			 * 获取群用户（where block flag is false）
+			 */
 			var user = groupUsers[i];
+			
 			if(user.connected){
 				// 对于在线用户，消息直接推送到用户的channel。
 				var msg = JSON.stringify({action: 'control', user: current.user, msg: ' joined the channel' });
