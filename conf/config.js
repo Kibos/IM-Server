8ca7a25f19579b809d843d2f0728da9fd37c350a
zmofei <13761509829@163.com>
2014-05-15 18:12:14 +0800

    fix bug

--- conf/config.js
+++ conf/config.js
@@ -3,50 +3,57 @@
  */
 
 exports.mongodb = {
-  // the defaulte mongodb
-  mg1: {
-    port: 27017,
-    ip: "10.21.3.59"
-  }
-}
+    // the defaulte mongodb
+    mg1: {
+        port: 27017,
+        ip: "10.21.3.59"
+    }
+};
 
 /**
  * the static host
  */
 exports.sta = {
-  // the plugpush server
-  PPSH: {
-    pp1: {
-      ip: '10.21.3.66',
-      port: '6379'
+    // the plugpush server
+    PPSH: {
+        pp1: {
+            ip: '10.21.3.66',
+            port: '6379'
+        }
+    },
+    redis: {
+        'token': {
+            ip: '10.21.3.59',
+            port: '6379',
+            select: '1'
+        }
+    },
+    group: {
+        'api': {
+            ip: '10.21.3.59',
+            port: '8888'
+        }
     }
-  },
-  redis: {
-    'token':{
-        ip:'10.21.3.59',
-        port:'6379',
-        select:'1'
-    }
-  }
 };
+
 
 /**
  *redis、node server
  */
 exports.Server = {
-  PNode: {},
-  PRedis: {
-    'pr1': {
-      'ip': '10.21.3.66',
-      'port': '6379'
-    }
-  },
-  GRedis: {
-    'gr1': {
-      'ip': '10.21.3.66',
-      'port': '6379'
-    }
-  },
-  GSub: {},
-  URedis: {}
+    PNode: {},
+    PRedis: {
+        'pr1': {
+            'ip': '10.21.3.66',
+            'port': '6379'
+        }
+    },
+    GRedis: {
+        'gr1': {
+            'ip': '10.21.3.66',
+            'port': '6379'
+        }
+    },
+    GSub: {},
+    URedis: {}
 };
