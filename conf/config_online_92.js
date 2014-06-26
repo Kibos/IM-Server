3ed8bf4411a8dc63f8e9c0baf50697160e7a4f62
zmofei <13761509829@163.com>
2014-06-26 13:30:32 +0800

    update

---
+++ conf/config_online_92.js
@@ -0,0 +1,66 @@
+/**
+ * mongodb
+ */
+exports.mongodb = {
+    // the defaulte mongodb
+    mg1: {
+        port: 27019,
+        ip: "10.21.3.44"
+    }
+};
+
+
+/**
+ * the static host
+ */
+
+exports.sta = {
+    redis: {
+        'token': {
+            ip: '10.21.3.42',
+            port: '6390',
+            select: '1'
+        },
+        'cache':{
+            ip: '10.21.3.42',
+            port: '6390'
+        }
+    },
+    //
+    group: {
+        'api': {
+            ip: '10.21.3.93',
+            port: '80'
+        }
+    },
+    friend: {
+        'api': {
+            ip: '10.21.3.93',
+            port: '80'
+        }
+    }
+};
+
+
+/**
+ *redis、node server
+ */
+exports.Server = {
+    PNode: {},
+    PRedis: {
+        'pr1': {
+            'ip': '10.21.3.42',
+            'port': '6391'
+        }
+    },
+    GRedis: {
+        'gr1': {
+            'ip': '10.21.3.42',
+            'port': '6391'
+        }
+    },
+    GSub: {},
+    URedis: {}
+};
+
+exports.users = {};
